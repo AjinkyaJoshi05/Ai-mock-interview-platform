0860a4bf43db4409e6b85dc5151c09a596d9c65d
AjinkyaJoshi05 <aaj.ajinkya@gmail.com>
2026-04-15 20:10:03 +0530

    feat: Refactor server.js to serve static files and handle catch-all route for React app

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,28 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import app from "./app.js";
 import dns from 'node:dns';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
 dns.setDefaultResultOrder('ipv4first');
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Serve static files from Vite build
+app.use(
+  express.static(path.join(__dirname, "../frontend/dist"))
+);
+
+// Catch-all: send React app
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/dist/index.html")
+  );
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("AI Mock Interview Platform API is running 🚀");
 });
