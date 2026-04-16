8383e7555c81cd9102106cf22023d358e152a2a9
AjinkyaJoshi05 <aaj.ajinkya@gmail.com>
2026-04-16 13:21:23 +0530

    fix: Configure CORS to allow requests from any origin

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ import interviewRoutes from "./routes/interviewRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/interview", interviewRoutes);
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,42 @@
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./.env" });
+// import app from "./app.js";
+// import dns from 'node:dns';
+// import express from "express";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+
+// dns.setDefaultResultOrder('ipv4first');
+// const PORT = process.env.PORT || 5000;
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// // Serve static files from Vite build
+// app.use(
+//   express.static(path.join(__dirname, "../frontend/dist"))
+// );
+
+// // Catch-all: send React app
+// app.use((req, res) => {
+//   res.sendFile(
+//     path.join(__dirname, "../frontend/dist/index.html")
+//   );
+// });
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+
 import app from "./app.js";
-import dns from 'node:dns';
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import dns from "node:dns";
 
+dns.setDefaultResultOrder("ipv4first");
 
-dns.setDefaultResultOrder('ipv4first');
 const PORT = process.env.PORT || 5000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// Serve static files from Vite build
-app.use(
-  express.static(path.join(__dirname, "../frontend/dist"))
-);
-
-// Catch-all: send React app
-app.use((req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../frontend/dist/index.html")
-  );
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
